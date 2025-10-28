0909ff6ef00d890f1bff35c47ffbd028166f368e
Fernando <josefernandosilva0501@gmail.com>
2025-10-28 00:59:33 +0000

    adiciona os script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para estudos do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1>Eu te amo Felipe UwU !!!!⸜(｡˃ ᵕ ˂ )⸝♡</h1>
+      <img src="/edward.jpg"></img>
+    </>
+  );
+}
 
-function Home(){
-    return<>
-    <h1>Eu te amo Felipe UwU !!!!⸜(｡˃ ᵕ ˂ )⸝♡</h1> 
-    <img src="/edward.jpg" ></img></>
-  
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
